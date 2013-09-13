e1657545c17c842a9d31a9d909b09b9d042484c7
Erik Royall <erikroyall@hotmail.com>
2013-09-13 19:52:29 +0530

    Deleted the previous files

--- src/legacy.js
+++
@@ -1,9 +0,0 @@
-
-  select = feature.qsa3 ? function (selector, root) {
-    // Set root to given root or document
-    root = root || doc;
-
-    return root.querySelectorAll(selector);
-  } : function (selector, root) {
-    return sizzle(selector, root);
-  };
