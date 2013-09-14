5e61753d3524fca5e3b2d174b4ccf135321b03b3
Erik Royall <erikroyall@hotmail.com>
2013-09-14 20:03:34 +0530

    Unsuccessful almost useless fil

---
+++ build/hilo-legacy.js
@@ -0,0 +1,9 @@
+// ========================= 
+// Hilo - 0.1.0-pre-dev-beta-10
+// ========================= 
+// 2013-09-14
+// Project started before 2 months and 14 days
+// http://erikroyall.github.com/hilo/
+// Copyright (c) 2013 Erik Royall
+// Licensed under MIT (see LICENSE-MIT) 
+
