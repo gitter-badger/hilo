9da9daf643d3912233727ee0785d90482a5d1ece
Erik Royall <erikroyall@hotmail.com>
2013-09-18 09:14:00 +0530

    Write tests for Hilo.dom.getByClassName

--- test/spec/hilo/dom.spec.js
+++ test/spec/hilo/dom.spec.js
@@ -12,4 +12,17 @@ describe("Hilo.dom", function () {
       expect(Hilo.dom.getById("")).toEqual([null]);
     });
   });
+  describe(".getByClassName", function () {
+    it("should get elements by id", function () {
+      document.body.className += " someClass";
+
+      expect(Hilo.dom.getByClassName("someClass")).toEqual(document.getElementsByClassName("someClass"));
+    });
+    it("shoud return an empty array with null when no id is passed", function () {
+      expect(Hilo.dom.getByClassName()).toEqual({ length: 0 });
+    });
+    it("should return an empty array when empty string is passed", function () {
+      expect(Hilo.dom.getByClassName("")).toEqual({ length: 0 });
+    });
+  });
 });
