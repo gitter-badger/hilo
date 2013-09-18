6b34e766e0b8770e078175c26f057c60dae50e94
Erik Royall <erikroyall@hotmail.com>
2013-09-19 01:40:46 +0530

    Write basic .on, .off and .fire methods

---
+++ src/hilo/event.js
@@ -0,0 +1,107 @@
+  
+  /**
+   * @static
+   * @class dom
+   * @for Hilo
+   */
+  var event = (function () {
+
+    // ### Implementation Notes
+    // 
+    // 1. If addEventListener, removeEventListener and dispatchEvent:
+    //    Use them, they work fine together
+    // 2. If addEventListener and removeEventListener but no dispatchEvent:
+    //    Store the event in a cache and trigger it when fired
+    // 3. If attachEvent, detachEvent:
+    //    Use them
+    // 4. If nothing
+    //    Manually attach the events el[on + evt] style
+    // 
+
+    // The handlers
+    var handlers = {};
+
+    // addEvent
+    var addEvent = (function () {
+      if (typeof doc.addEventListener === "function") {
+        return function (el, evt, fn) {
+          el.addEventListener(evt, fn, false);
+          handlers[el][evt] = handlers[el][evt] || [];
+          handlers[el][evt].push(fn);
+
+        };
+      } else if (typeof doc.attachEvent === "function") {
+        return function (el, evt, fn) {
+          el.attachEvent(evt, fn);
+          handlers[el][evt] = handlers[el][evt] || [];
+          handlers[el][evt].push(fn);
+        };
+      } else {
+        return function (el, evt, fn) {
+          el["on" + evt] = fn;
+          handlers[el][evt] = handlers[el][evt] || [];
+          handlers[el][evt].push(fn);
+        };
+      }
+    }());
+
+    // removeEvent
+    var removeEvent = (function () {
+      if (typeof doc.removeEventListener === "function") {
+        return function (el, evt, fn) {
+          el.removeEventListener(evt, fn, false);
+          Helio.each(handlers[el][evt], function (fun) {
+            if (fun === fn) {
+              handlers[el][evt][handlers[el][evt].indexOf(fun)] = undefined;
+            }
+          });
+
+        };
+      } else if (typeof doc.detachEvent === "function") {
+        return function (el, evt, fn) {
+          el.detachEvent(evt, fn);
+          Helio.each(handlers[el][evt], function (fun) {
+            if (fun === fn) {
+              handlers[el][evt][handlers[el][evt].indexOf(fun)] = undefined;
+            }
+          });
+        };
+      } else {
+        return function (el, evt, fn) {
+          el["on" + evt] = undefined;
+          Helio.each(handlers[el][evt], function (fun) {
+            if (fun === fn) {
+              handlers[el][evt][handlers[el][evt].indexOf(fun)] = undefined;
+            }
+          });
+        };
+      }
+    }());
+
+    // triggerEvent
+    function triggerEvent (el, evt) {
+      Helio.each(handlers[el][evt], function (fn) {
+        fn();
+      });
+    }
+
+    return {
+      add: addEvent,
+      on: addEvent,
+      attach: addEvent,
+      subscribe: addEvent,
+      remove: removeEvent,
+      off: removeEvent,
+      detach: removeEvent,
+      unsubscribe: removeEvent,
+      trigger: triggerEvent,
+      fire: triggerEvent,
+      dispatch: triggerEvent,
+      publish: triggerEvent
+    };
+  }());
+
+  // Provide event methods
+  Helio.extend(Hilo, {
+    event: event
+  });
